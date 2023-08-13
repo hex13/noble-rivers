286ed93ec90f5b24d8da36d9b3a581164797e0b0
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-08-13 19:43:49 +0200

    tile map

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log("Noble Rivers");
+
+const TILE_SIZE = 32;
+class Tile {
+    constructor() {
+        this.terrain = 'grass';
+    }
+}
+
+
+class TileMap {
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+        this.data = [...Array(w * h)].map(() => new Tile());
+    }
+    getIndex(pos) {
+        return pos.y * this.width + pos.x;
+    }
+    get(pos) {
+        return this.data[this.getIndex(pos)];
+    }
+}
